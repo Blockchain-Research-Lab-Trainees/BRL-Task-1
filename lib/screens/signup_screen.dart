ff339368a47ff0a978c5905be348fd52e30a1b2b
Euclid Stellar <euclidstellar@gmail.com>
2023-10-05 12:35:05 +0530

    SignUp is very Ideal 🫶🏻

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -11,10 +11,41 @@ class SignUp extends StatelessWidget {
         backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {
+              
+              Navigator.pop(context);
             
           },
-          icon: const Image(image: AssetImage('assets/images/back_arrow.png', ), height: 20, width: 20, fit: BoxFit.scaleDown,),),
-        title: const Text('Sign Up'),
+          icon: const Image(image: AssetImage('assets/images/back_arrow.png', ), width: 30, fit: BoxFit.scaleDown,),),
+        // title: const Text('Sign Up' , style: TextStyle(color: Color.fromARGB(255, 95, 95, 95),
+        // fontSize: 35,
+        // fontWeight: FontWeight.bold,
+        
+        // ),),
+      ),
+      body:  SafeArea(
+        child: Column(
+          children: [
+
+             const Center(
+               child: Text('Sign Up' , style: TextStyle(color: Color.fromARGB(255, 95, 95, 95),
+                     fontSize: 35,
+                     fontWeight: FontWeight.bold,
+                     
+                     ),),
+                     
+             ),
+
+             const SizedBox(
+              height: 20,),
+            
+              SizedBox(
+                width: double.infinity,
+                child: Image.asset('assets/images/img4.png'),
+              )
+
+          ],
+
+        )
       ),
     );
   }
