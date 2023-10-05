17ce0a5ad31b192add383a0ac9311533287a894c
Euclid Stellar <euclidstellar@gmail.com>
2023-10-06 02:31:18 +0530

    In mid of Sign Up Screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trainee_login/screens/forgot_password_screen.dart';
+import 'package:trainee_login/screens/home_screen.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -7,62 +9,242 @@ class SignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          reverse: true,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Padding(
-                      padding: EdgeInsets.only(left: 12.0),
-                      child: Image(
-                        image: AssetImage(
-                          'assets/images/back_arrow.png',
-                        ),
-                        width: 30,
-                        fit: BoxFit.scaleDown,
+          child: SingleChildScrollView(
+        reverse: true,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Padding(
+                    padding: EdgeInsets.only(left: 12.0),
+                    child: Image(
+                      image: AssetImage(
+                        'assets/images/back_arrow.png',
                       ),
+                      width: 30,
+                      fit: BoxFit.scaleDown,
                     ),
                   ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 65.0),
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 95, 95, 95),
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 65.0),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 95, 95, 95),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
+                ),
+              ],
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Image.asset(
+                'assets/images/img7.png',
+
+                fit: BoxFit.scaleDown,
+                //width: 300,
               ),
-              const SizedBox(
-                height: 5,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Image.asset(
-                  'assets/images/img7.png',
-                  fit: BoxFit.scaleDown,
-                  width: 300,
+            ),
+
+            const MySignUpButton(
+              hintText: 'Enter your Name',
+              inputType: TextInputType.name,
+              labelText2: 'Name',
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SignUpXd(
+                        buttonName: 'Sign Up with ',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomeScreenPage(
+                                title: 'homescreen',
+                              ),
+                            ),
+                          );
+                        },
+                        bgColor: Colors.black,
+                        textColor: Colors.white,
+                      ),
+                    ),
+                    
+                    // Padding(
+                    //   padding: const EdgeInsets.symmetric(
+                    //       horizontal: 8.0), 
+                    //   child: Image.asset(
+                    //     'assets/images/google.png', 
+                    //     width: 24, 
+                    //     height: 24, 
+                    //   ),
+                    // ),
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 20,
+            ),
+
+            // Padding(
+            //   padding: const EdgeInsets.all(20),
+            //   child: Container(
+            //     height: 60,
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(10),
+            //       color: Colors.white,
+            //     ),
+            //     child: Row(
+            //       children: [
+
+            //         Expanded(
+            //           child: SignUpXd(
+            //             buttonName: 'Sign Up with ',
+            //             onTap: () {
+            //               Navigator.push(
+            //                 context,
+            //                 MaterialPageRoute(
+            //                   builder: (context) => const HomeScreenPage(
+            //                     title: 'homescreen',
+            //                   ),
+            //                 ),
+            //               );
+            //             },
+
+            //             bgColor: Colors.black,
+            //             textColor: Colors.white,
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ForgotPassword(),
+                  ),
+                );
+              },
+              child: const Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 95, 95, 95),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ],
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+// MytextField Widget
+
+class MySignUpButton extends StatelessWidget {
+  const MySignUpButton({
+    super.key,
+    required this.hintText,
+    required this.inputType,
+    required this.labelText2,
+  });
+
+  final String hintText;
+  final TextInputType inputType;
+  final String labelText2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        keyboardType: inputType,
+        textInputAction: TextInputAction.next,
+        textCapitalization: TextCapitalization.words,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(20),
+          hintText: hintText,
+          hintStyle: const TextStyle(color: Colors.grey),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey, width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
           ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey, width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          labelText: labelText2,
+          labelStyle: const TextStyle(color: Colors.black54),
+        ),
+      ),
+    );
+  }
+}
+
+// Login Widget
+
+class SignUpXd extends StatelessWidget {
+  const SignUpXd({
+    Key? key,
+    required this.buttonName,
+    required this.onTap,
+    required this.bgColor,
+    required this.textColor,
+  }) : super(key: key);
+
+  final String buttonName;
+  final VoidCallback onTap;
+  final Color bgColor;
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: bgColor,
+      ),
+      child: TextButton(
+        style: ButtonStyle(
+          elevation: MaterialStateProperty.all(12),
+          shadowColor: MaterialStateProperty.all(Colors.black),
+          overlayColor: MaterialStateProperty.resolveWith(
+            (states) => Colors.transparent,
+          ),
+        ),
+        onPressed: onTap,
+        child: Text(
+          buttonName,
+          style: TextStyle(fontSize: 20, color: textColor),
         ),
       ),
     );
