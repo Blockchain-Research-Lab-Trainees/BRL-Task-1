1d1bc67fbab5593cd76841b790f7881a504590ee
Euclid Stellar <euclidstellar@gmail.com>
2023-10-09 19:50:28 +0530

    Google Sign In Completed

--- lib/services/firebase_auth.dart
+++ lib/services/firebase_auth.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:trainee_login/screens/home_screen.dart';
 import 'package:trainee_login/utils/showsnackbar.dart';
 
 class FirebaseAuthMethods {
@@ -59,22 +60,68 @@ Future<void> emailLogin({
     required String password,
     required BuildContext context,
   }) async {
-    try {
+     try {
       await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      if (!_auth.currentUser!.emailVerified) {
-        // ignore: use_build_context_synchronously
-        await sendEmailverifcation(context); 
-        // ignore: use_build_context_synchronously
-        showSnackBar(context, 'Please verify your email');
-        return;
+     
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Login successful'),
+          duration: Duration(seconds: 5), // Optional: Set duration
+        ),
+      );
+      if (_auth.currentUser!.emailVerified) {
+
+         Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreenPage(
+            title: 'homescreen',
+          ),
+        ),
+      );
+      
+        
+      } else {
+        
+       showSnackBar(context, 'Please verify your email');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+                'A link is sent to your email. Please verify your email address to continue'),
+            duration: Duration(seconds: 5), // Optional: Set duration
+          ),
+        );
       }
     } on FirebaseAuthException catch (e) {
-      showSnackBar(context, e.message!);
+      if (e.code == 'user-not-found') {
+        showSnackBar(context, 'No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        showSnackBar(context, 'Wrong password provided for that user.');
+      } else if (e.code == 'invalid-email') {
+       showSnackBar(context, 'Email is Invalid ');
+      } else if (e.code == 'user-disabled') {
+        showSnackBar(context, 'Account deleted');
+      } else if (e.code == 'too-many-requests') {
+       showSnackBar(context, 'Too many requests. Try again later.');
+      } else if (e.code == 'operation-not-allowed') {
+       showSnackBar(context, 'Signing in with Email and Password is not enabled.');
+      } else if (e.code == 'network-request-failed') {
+        showSnackBar(context, 'Please check your internet connection');
+      } else if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
+       showSnackBar(context, 'Invalid login credentials');
+      } else {
+       showSnackBar(context, e.code);
+      }
+    } catch (e) {
+     showSnackBar(context, e.toString()); 
+      // print(e);
     }
+  
   }
+
 
  
 // Email Verification
@@ -90,52 +137,97 @@ Future<void> emailLogin({
 
 
   // Google Sign In
-Future<void> signInWithGoogle(BuildContext context) async {
-    try {
+// Future<void> signInWithGoogle(BuildContext context) async {
+//     try {
 
-      final GoogleSignInAccount? googleSignInAccount =
-          await GoogleSignIn().signIn();
+//       final GoogleSignInAccount? googleSignInAccount =
+//           await GoogleSignIn().signIn();
 
-      final GoogleSignInAuthentication? googleSignInAuthentication = 
-          await googleSignInAccount?.authentication;
+//       final GoogleSignInAuthentication? googleSignInAuthentication = 
+//           await googleSignInAccount?.authentication;
 
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleSignInAuthentication?.accessToken,
-        idToken: googleSignInAuthentication?.idToken,
-      );
+//       final AuthCredential credential = GoogleAuthProvider.credential(
+//         accessToken: googleSignInAuthentication?.accessToken,
+//         idToken: googleSignInAuthentication?.idToken,
+//       );
 
-      //if ( googleSignInAuthentication?.accessToken != null && googleSignInAuthentication?.idToken != null) {
-        // final credential = GoogleAuthProvider.credential(
-        //   accessToken: googleSignInAuthentication?.accessToken,
-        //   idToken: googleSignInAuthentication?.idToken,
-        // );
-        // credential = GoogleAuthProvider.credential(
-        //   accessToken: googleSignInAuthentication?.accessToken,
-        //   idToken: googleSignInAuthentication?.idToken,
-        // );
+//       //if ( googleSignInAuthentication?.accessToken != null && googleSignInAuthentication?.idToken != null) {
+//         // final credential = GoogleAuthProvider.credential(
+//         //   accessToken: googleSignInAuthentication?.accessToken,
+//         //   idToken: googleSignInAuthentication?.idToken,
+//         // );
+//         // credential = GoogleAuthProvider.credential(
+//         //   accessToken: googleSignInAuthentication?.accessToken,
+//         //   idToken: googleSignInAuthentication?.idToken,
+//         // );
         
-        UserCredential userCredential =
-        await _auth.signInWithCredential(credential);
+//         UserCredential userCredential =
+//         await _auth.signInWithCredential(credential);
 
-        if (userCredential.user != null) {
-          if (userCredential.additionalUserInfo!.isNewUser) {
-            await sendEmailverifcation(context);
-            showSnackBar(context, 'Please verify your email');
-            return;
-          }
-        }
+//         if (userCredential.user != null) {
+//           if (userCredential.additionalUserInfo!.isNewUser) {
+//             await sendEmailverifcation(context);
+//             showSnackBar(context, 'Please verify your email');
+//             return;
+//           }
+//         }
 
-        // if (!_auth.currentUser!.emailVerified) {
-        //   await sendEmailverifcation(context);
-        //   showSnackBar(context, 'Please verify your email');
-        //   return;
-        // }
-    //  }
+//         // if (!_auth.currentUser!.emailVerified) {
+//         //   await sendEmailverifcation(context);
+//         //   showSnackBar(context, 'Please verify your email');
+//         //   return;
+//         // }
+//     //  }
     
-    }on FirebaseAuthException catch (e) {
-      showSnackBar(context, e.message!);
+//     }on FirebaseAuthException catch (e) {
+//       showSnackBar(context, e.message!);
+//     }
+//   } 
+
+
+Future<void> signInWithGoogle(BuildContext context) async {
+  try {
+    final GoogleSignInAccount? googleSignInAccount =
+        await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleSignInAuthentication =
+        await googleSignInAccount?.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication?.accessToken,
+      idToken: googleSignInAuthentication?.idToken,
+    );
+
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+    if (userCredential.user != null) {
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        await sendEmailverifcation(context);
+        showSnackBar(context, 'Please verify your email');
+      } else {
+        // Navigate to the home screen if authenticated
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreenPage(
+              title: 'homescreen',
+            ),
+          ),
+        );
+      }
+    } else {
+      // Handle the case where userCredential.user is null
+      showSnackBar(context, 'Authentication failed. Please try again.');
     }
-  } 
+  } on FirebaseAuthException catch (e) {
+    // Handle FirebaseAuthException errors
+    showSnackBar(context, e.message!);
+  } catch (e) {
+    // Handle other errors
+    showSnackBar(context, 'Authentication failed. Please try again.');
+  }
+}
 
 
   // anonymous sign in 
